056cbe3841257ea8ba36b0440559bb2aa8876cf4
Sanel Jasarevic <saneljasar199212@gmail.com>
2021-06-21 01:21:18 +0200

    dd

--- vue.config.js
+++ vue.config.js
@@ -4,9 +4,10 @@ module.exports = {
     // Creates dist folder at server directory 
     // outputDir: path.resolve(path.join(__dirname, "../server/dist")),
     devServer: {
-        proxy: 'http://localhost:5000'
+        proxy: 'https://shui-server.herokuapp.com'
     },
 };
 
 // Docker container 'http://server:5000'
 // Cloud: 'https://shui-server.herokuapp.com'
+//proxy: 'http://localhost:5000'
